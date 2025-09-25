caec9c1cbd08a453dab1605f6d0263d7ff10e8a7
InProcesss <sema22122007@gmail.com>
2025-09-25 09:45:02 +0200

    Add files via upload

---
+++ Project1.cpp
@@ -0,0 +1,16 @@
+﻿// Project1.cpp: определяет точку входа для приложения.
+//
+
+#include "Project1.h"
+
+using namespace std;
+
+int main()
+{	
+	int(a) = 4;
+	int(b) = 2;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	return 0;
+}
