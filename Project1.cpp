9a549d15ee5cfd2b716ad1b6e46098c214362d4b
InProcesss <233320435+InProcesss@users.noreply.github.com>
2025-09-26 19:35:33 +0200

    Add files via upload

--- Project1.cpp
+++ Project1.cpp
@@ -7,10 +7,33 @@ using namespace std;
 
 int main()
 {	
-	int(a) = 4;
-	int(b) = 2;
-	cout << a + b << endl;
-	cout << a - b << endl;
-	cout << a * b << endl;
+	setlocale(LC_ALL, "russian");
+	//вводим переменные double, так как double - тип данных с плавующей точкой(для деления)
+	double a, b, proizvedenie1, proizvedenie2, summa1, summa2, raznost1, raznost2, razmer_double, delenie1, delenie2;
+	cin >> a;
+	cin >> b;
+	razmer_double = sizeof(double); // sizeoff -- оператор для определения размера типа данных
+	/*проводим операции над переменными :
+	сложение
+	вычитание
+	усножение
+	*/
+	summa1 = a + b;
+	summa2 = b + a;
+	raznost1 = a - b;
+	raznost2 = b - a;
+	proizvedenie1 = a * b;
+	proizvedenie2 = b * a;
+	delenie1 = a / b;
+	delenie2 = b / a;
+	cout << "размер double" << razmer_double << endl;
+	cout << summa1 << " double" << endl;
+	cout << raznost1 << " double" << endl;
+	cout << proizvedenie1 << " double" << endl;
+	cout << summa2 << " double" << endl;
+	cout << raznost2 << " double" << endl;
+	cout << proizvedenie2 << " double" << endl;
+	cout << delenie1 << " double" << endl;
+	cout << delenie2 << " double" << endl;
 	return 0;
 }
