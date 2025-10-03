614c44bf00fc8b436748ae1b846ceead75c57b0a
InProcesss <233320435+InProcesss@users.noreply.github.com>
2025-10-03 22:51:39 +0200

    Update Project1.cpp

--- Project1.cpp
+++ Project1.cpp
@@ -8,11 +8,11 @@ using namespace std;
 int main()
 {	
 	setlocale(LC_ALL, "russian");
-	//вводим переменные double, так как double - тип данных с плавующей точкой(для деления)
+	//вводим переменные double, так как double - вещественный тип данных (для деления(делить на float нельзя))
 	double a, b, proizvedenie1, proizvedenie2, summa1, summa2, raznost1, raznost2, razmer_double, delenie1, delenie2;
 	cin >> a;
 	cin >> b;
-	razmer_double = sizeof(double); // sizeoff -- оператор для определения размера типа данных
+	razmer_double = sizeof(double); // sizeof -- оператор для определения размера типа данных
 	/*проводим операции над переменными :
 	сложение
 	вычитание
@@ -37,3 +37,4 @@ int main()
 	cout << delenie2 << " double" << endl;
 	return 0;
 }
+
